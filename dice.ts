0a42a06ea415764ed74b740b1c540f0b485bd1d7
Todd Bichoupan <tbcatan@gmail.com>
2025-12-20 13:17:36 -0500

    fixed dice interface

--- dice.ts
+++ dice.ts
@@ -1,5 +1,5 @@
 interface Dice {
   redDie: 1 | 2 | 3 | 4 | 5 | 6;
   yellowDie: 1 | 2 | 3 | 4 | 5 | 6;
-  eventDie: "barbarians" | "yellow" | "blue" | "green";
+  eventDie: "barbarians" | "yellow-progress" | "blue-progress" | "green-progress";
 }
